d0a320d1643fbe0fcfd02dd2ed29b80735ea0ff2
charvikkumarnv <charvikkumarnv@gmail.com>
2026-05-28 22:47:04 +0530

    Initialize Flask app with chat functionality

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, render_template, request
+from groq import Groq
+
+app = Flask(__name__)
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html", reply=reply)
+
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    message = request.form["message"]
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role":"user","content":message}
+        ]
+    )
+
+    reply = response.choices[0].message.content
+
+    return render_template("index.html", reply=reply)
+
+app.run(debug=True)
